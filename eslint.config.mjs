21553c8606c6afa1176958ef33a4545a9e672a99
Arash Layeghi <arash.layeghi@gmail.com>
2026-02-13 13:59:27 +0000

    chore: fix ESLint config deprecation and format pass

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,11 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 
-export default tseslint.config(
+export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   prettierConfig,
   {
     ignores: ['dist/', 'node_modules/'],
   },
-);
+];
